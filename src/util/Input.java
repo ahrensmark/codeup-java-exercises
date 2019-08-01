1f956e4d0ff53e5c343b3bbb91dd845d3ff33c14
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-08-01 04:47:10 -0500

    oop input validation exercise updated

--- src/util/Input.java
+++ src/util/Input.java
@@ -1,48 +1,77 @@
-//package util;
-//
-//import java.util.Scanner;
-//
-//public class Input {
-//
-//    private Scanner scanner;
-//
-//    public Input() {
-//        scanner = new Scanner(System.in);
-//    }
-//
-//    String getString() {
-//
-//    }
-//
-//    boolean yesNo() {
-//        String answer = this.scanner.nextLine();
-//        if(answer.equalsIgnoreCase("y") || answer.equalsIgnoreCase("yes")){
-//            return true;
-//        } else {return false;
-//        }
-//    }
-//
-//    int getInt(int min, int max) {
-//
-//    }
-//
-//    int getInt() {
-//
-//    }
-//
-//    double getDouble(double min, double max) {
-//
-//    }
-//
-//    double getDouble() {
-//
-//    }
-//
-//
-//
-//
-//
-//    public static void main(String[] args) {
-//
-//    }
-//}
+package util;
+
+import java.util.Scanner;
+
+public class Input {
+
+    private Scanner scanner;
+
+    public Input(){
+        this.scanner = new Scanner(System.in);
+    }
+
+    public String getString() {
+        return this.scanner.nextLine();
+    }
+    public String getString(String prompt) {
+        System.out.println(prompt);
+        return this.getString();
+    }
+
+    public boolean yesNo() {
+        String answer = this.scanner.nextLine();
+        if (answer.equalsIgnoreCase("Y")
+                || answer.equalsIgnoreCase("Yes")) {
+            return true;
+        } else return false;
+    }
+    public boolean yesNo(String prompt) {
+        System.out.println(prompt);
+        return this.yesNo();
+    }
+
+    public int getInt(int min, int max) {
+        do {
+            System.out.printf("Enter a number between %d and %d : ", min, max);
+            int answer = this.scanner.nextInt();
+            if (answer >= min && answer <= max) {
+                return answer;
+            }
+        } while (true);
+    }
+
+    public int getInt() {
+        return this.scanner.nextInt();
+    }
+    public int getInt(String prompt) {
+        System.out.println(prompt);
+        return this.getInt();
+    }
+
+    public double getDouble(double min, double max) {
+        do {
+            System.out.printf("Enter a number between %d and %d : ", min, max);
+            double answer = this.scanner.nextDouble();
+            if (answer >= min && answer <= max) {
+                return answer;
+            }
+        } while (true);
+
+    }
+    public double getDouble() {
+        return this.scanner.nextDouble();
+    }
+    public double getDouble(String prompt) {
+        System.out.println(prompt);
+        return this.getDouble();
+    }
+
+    public static void main(String[] args) {
+        Input input = new Input();
+        System.out.println("Enter your answer (Y/N)?");
+        System.out.println(input.yesNo());
+        System.out.println(input.getInt(1,10));
+    }
+}
+
+
