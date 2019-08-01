01257769f487b3005893a41def25c7a162e7e09b
Mark Ahrens <ahrens.mark.a@gmail.com>
2019-08-01 05:09:57 -0500

    oop calculating area & circumference of circle exercise

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -1,16 +1,15 @@
-//package shapes;
-//
-//public class CircleApp {
-//
-//    private double radius;
-//
-//    public Circle(double radius) {
-//        this.radius = radius;
-//    }
-//    public double getArea() {
-//        return Math.PI * this.radius + this.radius;
-//    }
-//    public double getCircumference() {
-//        return 2 * Math.PI * this.radius;
-//    }
-//}
+package shapes;
+
+import util.Input;
+
+public class CircleApp {
+    public static void main(String[] args) {
+        Input input = new Input();
+        double radius = input.getDouble("What is the radius of this circle?");
+        System.out.println("What is the radius of this circle (0 to 10)?");
+       // double radius = input.getDouble(0,1000);
+        Circle ourFunCircle = new Circle(radius);
+        System.out.printf("Area of circle = %f%n", ourFunCircle.getArea());
+        System.out.printf("Circumference of circle = %f%n", ourFunCircle.getCircumference());
+    }
+}
